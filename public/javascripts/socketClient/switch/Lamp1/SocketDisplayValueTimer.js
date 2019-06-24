762b215b9d75ce598a2967708ac1d2a5cb903a8e
Ngô văn việt <44596526+vanvietngo@users.noreply.github.com>
2019-06-24 08:14:45 +0700

    make it better

--- public/javascripts/socketClient/switch/Lamp1/SocketDisplayValueTimer.js
+++ public/javascripts/socketClient/switch/Lamp1/SocketDisplayValueTimer.js
@@ -2,15 +2,20 @@ var socket = io.connect('http://vanvietonline.herokuapp.com/');
 
 
 socket.on("Server-send-Timeout", function () {
-    $(".value-timer-lamp-1").html("Time out").hide(4000);
+    // $(".value-timer-lamp-1").html("Time out").hide(4000);
+    console.log('Server-send-Timeout');
 });
 
 
 socket.on("Server-send-ValueTimer", function (data) {
-  $(".value-timer-lamp-1").html("After  " + data + " s Lamp OFF").show();
+  // $(".value-timer-lamp-1").html("After  " + data + " s Lamp OFF").show();
+  console.log('Server-send-ValueTimer');
+
 });
 
 
 socket.on("Server-send-Cancel-Timer", function () {
-$(".value-timer-lamp-1").html("Canceled").hide(4000);
+// $(".value-timer-lamp-1").html("Canceled").hide(4000);
+console.log('Server-send-Cancel-Timer');
+
 });
