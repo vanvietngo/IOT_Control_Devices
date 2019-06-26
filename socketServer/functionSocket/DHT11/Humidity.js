50674e7f97e34965339da71e4da10944b4d57846
Ngô văn việt <44596526+vanvietngo@users.noreply.github.com>
2019-06-26 18:40:38 +0700

    make it better

--- socketServer/functionSocket/DHT11/Humidity.js
+++ socketServer/functionSocket/DHT11/Humidity.js
@@ -1,12 +1,12 @@
 module.exports = {
 
-    Humidity: (socket, io, arr, data) => {
+    Humidity: (socket, io, arr, dataModel) => {
         // Send news on the socket
         socket.on('DHT-Humidity', function (data) {
             io.sockets.emit("Humidity-client", data);
             arr.push(data.Humidity);
             // add data to mongo by arr
-            datta.create({
+            dataModel.create({
               time: arr[0],
               Temperature: arr[1],
               Humidity:arr[2]
