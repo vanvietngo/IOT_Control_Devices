72fa4323f36b529a81314c19c693c190ec7f81dc
Ngô văn việt <44596526+vanvietngo@users.noreply.github.com>
2019-06-26 19:02:42 +0700

    make it better

--- socketServer/functionSocket/DHT11/Temperature.js
+++ socketServer/functionSocket/DHT11/Temperature.js
@@ -6,6 +6,7 @@ module.exports = {
             arr.push(Date());
             arr.push(data.Temperature);
             io.sockets.emit("Temperature-client", data);
+            console.log("Arr 1111  = " + arr);
 
         });
     }
