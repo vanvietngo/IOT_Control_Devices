2612e075e56dcdb6ff901000be55f81ce9a2b45c
Ngô văn việt <44596526+vanvietngo@users.noreply.github.com>
2019-06-27 17:28:07 +0700

    make it better

--- socketServer/functionSocket/Show-Warning/Thief.js
+++ socketServer/functionSocket/Show-Warning/Thief.js
@@ -2,8 +2,8 @@
 module.exports = {
   Thief: (socket, io)=>{
         // Send news on the socket
-        socket.on('client-send-Thief', function (data) {
-            io.sockets.emit("server-send-rainning");
+        socket.on('client-ESP-send-Thief', function (data) {
+            io.sockets.emit("server-send-Thief");
         });
 
         // socket2.on('client-send-lamp2-off', function (data) {
