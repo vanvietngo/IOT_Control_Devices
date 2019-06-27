8aa47e5eae0001fb47783ea8d5c6c1e041ba2bd9
Ngô văn việt <44596526+vanvietngo@users.noreply.github.com>
2019-06-27 07:29:43 +0700

    make it better

--- public/javascripts/socketClient/switch/Lamp1/OnOffLamp.js
+++ public/javascripts/socketClient/switch/Lamp1/OnOffLamp.js
@@ -2,31 +2,33 @@ var socket = io.connect('http://vanvietonline.herokuapp.com/');
 
       // Hàm show kết quả
 
-      $("#btn1").click( function(){
+      $("#btn1").click( function(i){
       if( $(this).is(':checked') ){
         socket.emit("client-send-lamp1-on", "on");
-        var i = 100
-        while(i){
-          $("#someElement").fadeOut(100).fadeIn(100);
-          i-=1;
-        }
 
-        console.log("staus lamp1 on");
+        var myStopFunction = setInterval(function(){
+          $("#someElement").fadeOut(100).fadeIn(100);
+        }, 500);
+
+        socket.on('server-send-lamp1-off', function (data) {
+          clearInterval(myStopFunction);
+        });
+
+
       } else{
         socket.emit("client-send-lamp1-off", "off");
-        console.log("status lamp1 ==> off")
       }
       });
 
       // Send news on the socket
       socket.on('server-send-lamp1-on', function (data) {
         $("#btn1").prop("checked", true);
-          console.log(data);
-          console.log("da nhan dc server-send-lamp1-on 1");
       });
 
       socket.on('server-send-lamp1-off', function (data) {
         $("#btn1").prop("checked", false);
-          console.log(data);
-          console.log("da nhan dc server-send-lamp1-off 1");
+        i=0;
+
+        console.log('i = ' + i)
+
       });
