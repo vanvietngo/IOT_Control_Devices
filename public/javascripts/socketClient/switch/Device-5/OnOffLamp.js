107a9fa39e6c9a9fc3fc476a3f3f1285edd7b865
Ngô văn việt <44596526+vanvietngo@users.noreply.github.com>
2019-06-28 00:05:58 +0700

    make it better

--- public/javascripts/socketClient/switch/Device-5/OnOffLamp.js
+++ public/javascripts/socketClient/switch/Device-5/OnOffLamp.js
@@ -2,19 +2,19 @@ var socket = io.connect('http://vanvietonline.herokuapp.com/');
 
       // Hàm show kết quả
 
-      $("#btn1").click( function(i){
+      $("#btn5").click( function(i){
       if( $(this).is(':checked') ){
-        socket.emit("client-send-lamp1-on", "on");
+        socket.emit("client-send-lamp5-on", "on");
       } else{
-        socket.emit("client-send-lamp1-off", "off");
+        socket.emit("client-send-lamp5-off", "off");
       }
       });
 
       // Send news on the socket
-      socket.on('server-send-lamp1-on', function (data) {
-        $("#btn1").prop("checked", true);
+      socket.on('server-send-lamp5-on', function (data) {
+        $("#btn5").prop("checked", true);
       });
 
-      socket.on('server-send-lamp1-off', function (data) {
-        $("#btn1").prop("checked", false);
+      socket.on('server-send-lamp5-off', function (data) {
+        $("#btn5").prop("checked", false);
       });
--- public/javascripts/socketClient/switch/Device-5/SocketDisplayValueTimer.js
+++ public/javascripts/socketClient/switch/Device-5/SocketDisplayValueTimer.js
@@ -1,16 +1,16 @@
 var socket = io.connect('http://vanvietonline.herokuapp.com/');
 
 
-socket.on("Server-send-Timeout-1", function () {
-    $(".value-timer-lamp-1").html("Time out").hide(4000);
+socket.on("Server-send-Timeout-5", function () {
+    $(".value-timer-lamp-5").html("Time out").hide(4000);
 });
 
 
 socket.on("Server-send-ValueTimer-1", function (data) {
-  $(".value-timer-lamp-1").html("After  " + data + " s Lamp OFF").show();
+  $(".value-timer-lamp-5").html("After  " + data + " s Lamp OFF").show();
 });
 
 
-socket.on("Server-send-Cancel-Timer-1", function () {
-$(".value-timer-lamp-1").html("Canceled").hide(4000);
+socket.on("Server-send-Cancel-Timer-5", function () {
+$(".value-timer-lamp-5").html("Canceled").hide(4000);
 });
