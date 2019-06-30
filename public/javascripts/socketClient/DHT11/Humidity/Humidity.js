a588fbcdb7424e46c0708ec29bc15096f3b4156a
Ngô văn việt <44596526+vanvietngo@users.noreply.github.com>
2019-06-30 17:50:20 +0700

    make it better

--- public/javascripts/socketClient/DHT11/Humidity/Humidity.js
+++ public/javascripts/socketClient/DHT11/Humidity/Humidity.js
@@ -2,8 +2,10 @@ var socket = io.connect('http://vanvietonline.herokuapp.com/');
 // lay time tu node luon .
 
 socket.on("Humidity-client", function (data) {
+  var now = new Date();
+
     $(".da")
         .html(data.Humidity + " %");
     $(".timee")
-        .html("<br>"+Date());
+        .html("<br>"+new Date(now + " GMT-0700").toUTCString());
 });
