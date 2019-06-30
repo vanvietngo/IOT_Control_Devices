15a874b1efd0c0b042ff4ed171ce1282e3bc3b61
Ngô văn việt <44596526+vanvietngo@users.noreply.github.com>
2019-06-30 11:22:01 +0700

    make it better

--- socketServer/functionSocket/Save-data-Device/Device-1.js
+++ socketServer/functionSocket/Save-data-Device/Device-1.js
@@ -2,19 +2,21 @@ module.exports = {
     SaveDevice1: (socket, io, modelDevice1) => {
       socket.on('client-send-save-to-DB-of-Device-1', function (data) {
       var infor = data;
+
       modelDevice1.create({
         Time:Date(),
         DeviceName:"Device 1",
         User: data.user,
         Status: data.status,
         // Timer:String
-      })
+      });
+
       io.sockets.emit("Server-send-save-to-DB-of-Device-1");
       console.log(infor.user);
       console.log(infor.status);
-      socket.on('Esp-confirm-save-data-for-device-1', function () {
-        // create to database
-      }
-    }
+      // socket.on('Esp-confirm-save-data-for-device-1', function () {
+      //   // create to database
+      // }
+    });
   }
 };
