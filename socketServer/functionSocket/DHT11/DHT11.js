59738a45cf63f2470e0c14f2d647a36d5a94d9df
Ngô văn việt <44596526+vanvietngo@users.noreply.github.com>
2019-06-30 23:35:25 +0700

    make it better

--- socketServer/functionSocket/DHT11/DHT11.js
+++ socketServer/functionSocket/DHT11/DHT11.js
@@ -32,17 +32,23 @@ module.exports = {
                 else {
                   console.log(results[0].Temperature);
                   console.log("arr[1]" + arr[1]);
+                  dataModel.create({
+                      // console.log("da create data")
+                      Time: arr[0],
+                      Temperature: arr[1],
+                      Humidity: arr[2]
+                  });
 
-                    if(results[0].Temperature != arr[1] | results[0].Humidity !=arr[2]){
-                      console.log(results[0].Temperature);
-                      dataModel.create({
-                          // console.log("da create data")
-                          Time: arr[0],
-                          Temperature: arr[1],
-                          Humidity: arr[2]
-                      });
-                      arr = [];
-                    }
+                    // if(results[0].Temperature != arr[1] | results[0].Humidity !=arr[2]){
+                    //   console.log(results[0].Temperature);
+                    //   dataModel.create({
+                    //       // console.log("da create data")
+                    //       Time: arr[0],
+                    //       Temperature: arr[1],
+                    //       Humidity: arr[2]
+                    //   });
+                    //   arr = [];
+                    // }
                 }
             });
             // var tempSS = dataModel.findOne().sort({
