c003c570f02c61a6d80fe0ca6071e08db11bccff
Ngô văn việt <44596526+vanvietngo@users.noreply.github.com>
2019-07-02 07:37:16 +0700

    make it better

--- public/javascripts/socketClient/infor-User/index.js
+++ public/javascripts/socketClient/infor-User/index.js
@@ -4,16 +4,15 @@ $(document).ready(function(){
   socket.emit("Client-send-infor-User-Online",   {
     user: $('#UserName').text(),
     avatar:$('.avatarUser').attr('src')
-  }
+  });
+}
   console.log('UserName.text = ' + $('#UserName').text());
   console.log('avatarUser = ' + $('#UserName').text())$('.avatarUser').attr('src');
 
-);
+
 
   socket.on("Server-send-list-infor-User-Online", function (data) {
     data.forEach(function(i){
       $(".ListUserOnline").append("<img src=" + i +" alt='Avatar' class='avatar dropdown-toggle'id='dropdownMenu2' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>");
     });
   });
-
-}
