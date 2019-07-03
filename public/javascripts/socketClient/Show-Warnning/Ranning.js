e4a9f4ab0b70f557ff59b8602f7d6683295a0545
Ngô văn việt <44596526+vanvietngo@users.noreply.github.com>
2019-07-03 21:55:45 +0700

    make it better

--- public/javascripts/socketClient/Show-Warnning/Ranning.js
+++ public/javascripts/socketClient/Show-Warnning/Ranning.js
@@ -1,12 +1,12 @@
 socket.on('server-send-rainning', function () {
   var beepWarning = $("#beep-warning")[0];
-  beepWarning.play();
   var temp_Cancel = 0;
   var now = new Date();
   $('.Time-Rain').html('<br><br>' + new Date(now + " GMT-0000").toUTCString());
   var myStop_Function_Rainning = setInterval(function(){
     temp_Cancel += 1;
     $(".class-Rainning").fadeOut(100).fadeIn(100);
+    beepWarning.play();
 
 
 
