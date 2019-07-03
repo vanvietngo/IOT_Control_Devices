82d36ce5b3359e44670f081f86227a03ca838f6b
Ngô văn việt <44596526+vanvietngo@users.noreply.github.com>
2019-07-03 22:20:31 +0700

    make it better

--- public/javascripts/socketClient/Show-Warnning/Ranning.js
+++ public/javascripts/socketClient/Show-Warnning/Ranning.js
@@ -1,12 +1,12 @@
 socket.on('server-send-rainning', function () {
-  var beepWarning = $("#beep-warning")[0];
+  // var beepWarning = $("#beep-warning")[0];
   var temp_Cancel = 0;
   var now = new Date();
   $('.Time-Rain').html('<br><br>' + new Date(now + " GMT-0000").toUTCString());
   var myStop_Function_Rainning = setInterval(function(){
     temp_Cancel += 1;
     $(".class-Rainning").fadeOut(100).fadeIn(100);
-    beepWarning.play();
+    // beepWarning.play();
 
     if(temp_Cancel > 20){
 // blink 11s  after recive signal for esp
--- public/javascripts/socketClient/Show-Warnning/Thief.js
+++ public/javascripts/socketClient/Show-Warnning/Thief.js
@@ -1,7 +1,11 @@
 socket.on('server-send-Thief', function () {
+  var beepWarning = $("#beep-warning")[0];
+
   var now = new Date();
   $('.Time-Thief').html('<br><br>' + new Date(now + " GMT-0000").toUTCString());
     var myStop_Function_Thief = setInterval(function(){
+      beepWarning.play();
+
     $(".class-Thief").fadeOut(100).fadeIn(100);
   }, 500);
 
