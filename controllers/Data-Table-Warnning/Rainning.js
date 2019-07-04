172f168f47fdebfcc249947fd225d0e911471717
Ngô văn việt <44596526+vanvietngo@users.noreply.github.com>
2019-07-04 14:14:30 +0700

    make it better

--- controllers/Data-Table-Warnning/Rainning.js
+++ controllers/Data-Table-Warnning/Rainning.js
@@ -1,30 +1,63 @@
-const Data_Rain = require('../../models/Warnning-model/Rainning-model');
-// const DataDHT = require('../../models/DHT-model/index');
+// const Data_Rain = require('../../models/Warnning-model/Rainning-model');
+const DataDHT = require('../../models/DHT-model/index');
+//
+// module.exports={
+//   index2: (req, res, next)=>{
+//     var arrTime2 = [];
+//     var arrStatus2 = [];
+//     // var arrUser_turn_off_Warnning = [];
+//
+//     var element2 = {};
+//
+//
+//     Data_Rain.Ranning.find().then((data)=>{
+//         for (var i in data){
+//           arrTime2.push(data[i].Time);
+//           arrStatus2.push(data[i].Status);
+//           // arrUser_turn_off_Warnning.push(data[i].User_turn_off_Warnning);
+//         }
+//
+//     element2 = {
+//       user:req.user,
+//       Time:arrTime2,
+//       Status:arrStatus2
+//       // User_turn_off_Warnning:arrUser_turn_off_Warnning
+//     }
+//         // res.status(200).send(element);
+//         res.render('pages/dataTable/Warnning/Raining', {Data:element2});
+//
+//
+//       })
+//   }
+// }
+
+
+// const DataDHT = require('../models/DHT-model/index');
 
 module.exports={
-  index2: (req, res, next)=>{
-    var arrTime2 = [];
-    var arrStatus2 = [];
-    // var arrUser_turn_off_Warnning = [];
+  index: (req, res, next)=>{
+    var arrTime = [];
+    var arrTemp = [];
+    var arrHum = [];
 
-    var element2 = {};
+    var element = {};
 
 
-    Data_Rain.Ranning.find().then((data)=>{
+    DataDHT.Data.find().then((data)=>{
         for (var i in data){
-          arrTime2.push(data[i].Time);
-          arrStatus2.push(data[i].Status);
-          // arrUser_turn_off_Warnning.push(data[i].User_turn_off_Warnning);
+          arrTime.push(data[i].Time);
+          arrTemp.push(data[i].Temperature);
+          arrHum.push(data[i].Humidity);
         }
 
-    element2 = {
+    element = {
       user:req.user,
-      Time:arrTime2,
-      Status:arrStatus2
-      // User_turn_off_Warnning:arrUser_turn_off_Warnning
+      Time:arrTime,
+      Temperature:arrTemp,
+      Humidity:arrHum
     }
         // res.status(200).send(element);
-        res.render('pages/dataTable/Warnning/Raining', {Data:element2});
+                res.render('pages/dataTable/Warnning/Raining', {Data:element});
 
 
       })
