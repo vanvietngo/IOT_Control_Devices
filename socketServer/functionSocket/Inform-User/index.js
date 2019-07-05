a625be8d9319c162911e33e0963166556a2b18cf
Ngô văn việt <44596526+vanvietngo@users.noreply.github.com>
2019-07-05 09:33:55 +0700

    make it better

--- socketServer/functionSocket/Inform-User/index.js
+++ socketServer/functionSocket/Inform-User/index.js
@@ -4,24 +4,26 @@ module.exports = {
         socket.on('Client-send-infor-User-Online', function(data) {
             ArrUser.push(data.avatar);
             arrName.push(data.user);
+
 console.log('arrName = ' + arrName);
+
             socket.avatar = data.avatar;
-            socket.userr = data.userr;
+            socket.user = data.user;
             io.sockets.emit("Server-send-list-infor-User-Online", {
               ArrUser:ArrUser,
               arrName:arrName
             });
         });
         socket.on('disconnect', function() {
-            console.log('arr = ' + ArrUser);
+            console.log('arr before dis nu,ber = ' + arrName.length + " === >> " + ArrUser.length);
             if(socket.avatar != null) {
                 ArrUser.splice(ArrUser.indexOf(socket.avatar), 1);
-                arrName.splice(arrName.indexOf(socket.userr), 1);
+                arrName.splice(arrName.indexOf(socket.user), 1);
                 socket.broadcast.emit("Server-send-list-infor-User-Online", {
                   ArrUser:ArrUser,
                   arrName:arrName
                 });
-            console.log('arr dis = ' + arrName)    
+            // console.log('arr dis = ' + arrName)
             }
         });
     }
