727068a88878965e83307638aeb997dfaa1b2bb5
Ngô văn việt <44596526+vanvietngo@users.noreply.github.com>
2019-07-05 10:37:24 +0700

    make it better

--- public/javascripts/socketClient/infor-User/index.js
+++ public/javascripts/socketClient/infor-User/index.js
@@ -9,14 +9,30 @@ $(document).ready(function() {
 socket.on("Server-send-list-infor-User-Online", function(data) {
     $(".ListUserOnline").html("");
     data.ArrUser.forEach(function(i) {
+      console.log(data)
+      console.log('i = ' + [data.ArrUser.indexOf(i)]);
+      console.log('arrName(i) = ' + [data.ArrUser.indexOf(i)] + "  " + data.arrName[data.ArrUser.indexOf(i)]);
       // console.log("data.arrUser[ArrUser.indexOf(i)] = "+data.arrUser[data.ArrUser.indexOf(i)]);
       // console.log('so = ' + data.ArrUser.indexOf(i));
         $(".ListUserOnline").append("<img src=" + i +
         " alt='Avatar' class='img-thumbnail avatar dropdown-toggle'id='dropdownMenu2' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>"
-      // +"  <div class='dropdown-menu' aria-labelledby='dropdownMenu2'>"
-        // +"  <button class='dropdown-item' type='button'>"+
-          // data.arrUser[data.ArrUser.indexOf(i)]
-        // +"</button>"
-      // +"  </div>");
-    )});
+      +"  <div class='dropdown-menu' aria-labelledby='dropdownMenu2'>"
+        +"  <button class='dropdown-item' type='button'>"+
+          data.arrName[data.ArrUser.indexOf(i)]
+        +"</button>"
+      +"  </div>"
+    )
+  });
+
+// for(i = 0; i < data.ArrUser.length;i++){
+//   $(".ListUserOnline").append("<img src=" + data.ArrUser[i] +
+//   " alt='Avatar' class='img-thumbnail avatar dropdown-toggle'id='dropdownMenu2' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>"
+// +"  <div class='dropdown-menu' aria-labelledby='dropdownMenu2'>"
+//   +"  <button class='dropdown-item' type='button'>"+
+//     data.arrUser[data.ArrUser.indexOf(i)]
+//   +"</button>"
+// +"  </div>"
+// )
+// }
+
 });
