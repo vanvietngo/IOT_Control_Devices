85e863e8abb7fd1dd14bd9f2cf282f8801f09300
Ngô văn việt <44596526+vanvietngo@users.noreply.github.com>
2019-07-05 10:05:50 +0700

    make it better

--- public/javascripts/socketClient/infor-User/index.js
+++ public/javascripts/socketClient/infor-User/index.js
@@ -9,14 +9,14 @@ $(document).ready(function() {
 socket.on("Server-send-list-infor-User-Online", function(data) {
     $(".ListUserOnline").html("");
     data.ArrUser.forEach(function(i) {
-      console.log("data.arrUser[ArrUser.indexOf(i)] = "+data.arrUser[data.ArrUser.indexOf(i)]);
-      console.log('so = ' + data.ArrUser.indexOf(i));
+      // console.log("data.arrUser[ArrUser.indexOf(i)] = "+data.arrUser[data.ArrUser.indexOf(i)]);
+      // console.log('so = ' + data.ArrUser.indexOf(i));
         $(".ListUserOnline").append("<img src=" + i +
         " alt='Avatar' class='img-thumbnail avatar dropdown-toggle'id='dropdownMenu2' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>"
-      +"  <div class='dropdown-menu' aria-labelledby='dropdownMenu2'>"
-        +"  <button class='dropdown-item' type='button'>"+
-          data.arrUser[data.ArrUser.indexOf(i)]
-        +"</button>"
-      +"  </div>");
-    });
+      // +"  <div class='dropdown-menu' aria-labelledby='dropdownMenu2'>"
+        // +"  <button class='dropdown-item' type='button'>"+
+          // data.arrUser[data.ArrUser.indexOf(i)]
+        // +"</button>"
+      // +"  </div>");
+    )});
 });
--- socketServer/functionSocket/Inform-User/index.js
+++ socketServer/functionSocket/Inform-User/index.js
@@ -5,7 +5,7 @@ module.exports = {
             ArrUser.push(data.avatar);
             arrName.push(data.user);
 
-console.log('arrName = ' + arrName);
+// console.log('arrName = ' + arrName);
 
             socket.avatar = data.avatar;
             socket.user = data.user;
@@ -15,7 +15,7 @@ console.log('arrName = ' + arrName);
             });
         });
         socket.on('disconnect', function() {
-            console.log('arr before dis nu,ber = ' + arrName.length + " === >> " + ArrUser.length;
+            // console.log('arr before dis number = ' + arrName.length + " === >> " + ArrUser.length;
             if(socket.avatar != null) {
                 ArrUser.splice(ArrUser.indexOf(socket.avatar), 1);
                 arrName.splice(arrName.indexOf(socket.user), 1);
