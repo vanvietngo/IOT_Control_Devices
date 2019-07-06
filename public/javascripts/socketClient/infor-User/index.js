813b31ed5165a88f7c610811634aeedff1125fea
Ngô văn việt <44596526+vanvietngo@users.noreply.github.com>
2019-07-06 21:15:23 +0700

    make it better

--- public/javascripts/socketClient/infor-User/index.js
+++ public/javascripts/socketClient/infor-User/index.js
@@ -8,23 +8,9 @@ $(document).ready(function() {
 })
 socket.on("Server-send-list-infor-User-Online", function(data) {
     $(".each-User").remove();
-  //   data.ArrUser.forEach(function(i) {
-  //     console.log(data)
-  //     console.log('i = ' + [data.ArrUser.indexOf(i)]);
-  //     console.log('arrName(i) = ' + [data.ArrUser.indexOf(i)] + "  " + data.arrName[data.ArrUser.indexOf(i)]);
-  //     // console.log("data.arrUser[ArrUser.indexOf(i)] = "+data.arrUser[data.ArrUser.indexOf(i)]);
-  //     // console.log('so = ' + data.ArrUser.indexOf(i));
-  //       $(".ListUserOnline").append("<img src=" + i +
-  //       " alt='Avatar' class='img-thumbnail avatar dropdown-toggle'id='dropdownMenu2' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>"
-  //     +"  <div class='dropdown-menu' aria-labelledby='dropdownMenu2'>"
-  //       +"  <button class='dropdown-item' type='button'>"+
-  //         data.arrName[data.ArrUser.indexOf(i)]
-  //       +"</button>"
-  //     +"  </div>"
-  //   )
-  // });
-      console.log(data)
 
+      console.log(data)
+// display value name of each user
 for(i = 0; i < data.ArrUser.length;i++){
 var temp = data.arrName[i];
 $(".UserOnline").append("<div class='dropdown ava each-User'>"+
