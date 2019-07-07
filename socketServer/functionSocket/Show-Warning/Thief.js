6e4280c31d03c2427ff7e91e707b90e3502cc659
Ngô văn việt <44596526+vanvietngo@users.noreply.github.com>
2019-07-07 11:34:58 +0700

    make it better

--- socketServer/functionSocket/Show-Warning/Thief.js
+++ socketServer/functionSocket/Show-Warning/Thief.js
@@ -2,14 +2,14 @@
 module.exports = {
   Thief: (socket, io, dataModel, Send_Mail)=>{
         // Send news on the socket
-        socket.on('client-ESP-send-Thief', function (data) {
+        socket.on('client-ESP-send-Thief', function () {
+          var sendmail = Send_Mail;
           var now = new Date();
           dataModel.create({
             Time: new Date(now + " GMT-0700").toUTCString(),
             Status:'ON'
           });
             io.sockets.emit("server-send-Thief");
-            var sendmail = Send_Mail;
         });
   }
 
